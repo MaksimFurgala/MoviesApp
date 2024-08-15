a32918d872d238bc7375d61841f683d53ffb2c79
MaksimFurgala <furgala.maxim@yandex.ru>
2024-08-15 18:47:08 +0300

    added: - dependencies viewPager2, glide, chipNavigationBar, blurView; - IntroActivity; - background shapes for widgets; - slider and adapter. update: - strings.xml, themes.xml

---
+++ app/src/main/java/com/example/moviesapp/Activities/IntroActivity.java
@@ -0,0 +1,30 @@
+package com.example.moviesapp.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.moviesapp.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends AppCompatActivity {
+
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        binding.startBtn.setOnClickListener(v -> {
+            startActivity(new Intent(IntroActivity.this, MainActivity.class));
+        });
+    }
+}
